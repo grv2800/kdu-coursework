dcb604cf0029a3fce191721d423a3bac210cf077
grv2800 <gaurav.mehta@kickdrumtech.com>
2024-02-15 23:36:07 +0530

    fe-homework-8

---
+++ frontend/homework/8/typescript.js
@@ -0,0 +1,55 @@
+var SearchApp = /** @class */ (function () {
+    function SearchApp() {
+        this.recipes = [];
+    }
+    SearchApp.prototype.fetchRecipesFromAPI = function () {
+        var _this = this;
+        return fetch("https://dummyjson.com/recipes").then(function (response) { return response.json(); }).
+            then(function (data) {
+            console.log('data fetched from the api');
+            if (data && data.recipes) {
+                _this.recipes = data.recipes.map(function (recipe) { return ({
+                    image: recipe.image,
+                    name: recipe.name,
+                    rating: recipe.rating,
+                    cuisine: recipe.cuisine,
+                    ingredients: recipe.ingredients,
+                    difficulty: recipe.difficulty,
+                    timeTaken: recipe.prepTimeMinutes + recipe.cookTimeMinutes,
+                    calorieCount: recipe.caloriesPerServing
+                }); });
+                console.log('Recipes fetched from the API:', _this.recipes);
+            }
+            else {
+                console.log('No recipes found in the response');
+            }
+        })
+            .catch(function (error) {
+            console.log('error fetching recipes:', error);
+        });
+    };
+    SearchApp.prototype.searchRecipes = function (query) {
+        return fetch("https://dummyjson.com/recipes/search?q=".concat(query)).then(function (response) { return response.json(); }).
+            then(function (data) {
+            console.log('recipes fetched corresponding the given query', data);
+        })
+            .catch(function (error) {
+            console.log("error in fetching ", error);
+        });
+    };
+    SearchApp.prototype.printAllRecipes = function () {
+        this.recipes.map(function (recipe) {
+            console.log("image:", recipe.image);
+            console.log("name:", recipe.name);
+            console.log("rating:", recipe.rating);
+            console.log("cuisine:", recipe.cuisine);
+            console.log("ingredients:", recipe.ingredients);
+            console.log("difficulty:", recipe.difficulty);
+            console.log("time taken:", recipe.timeTaken);
+            console.log("calories:", recipe.calorieCount);
+        });
+    };
+    return SearchApp;
+}());
+var search = new SearchApp();
+search.fetchRecipesFromAPI().then(function () { return search.searchRecipes("Brazilian Caipirinha"); });
